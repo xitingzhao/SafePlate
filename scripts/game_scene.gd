14cc384e838e8ad9b62a2de14fb95d50c22fb1b5
xiting.zhao <tina102023@163.com>
2026-06-07 23:27:44 +0200

    person1 und person2 verbinden fertig

--- scripts/game_scene.gd
+++ scripts/game_scene.gd
@@ -5,7 +5,8 @@ extends Control
 func _ready() -> void:
 	print(GameGlobal.player1)
 	print(GameGlobal.player2)
-	
+	$Player1Area/ProfileArea1/VBoxContainer/ProfilArt.text = GameGlobal.player1
+	$Player2Area/ProfileArea2/VBoxContainer/ProflArt.text = GameGlobal.player2
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
