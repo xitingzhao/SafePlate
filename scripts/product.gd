c302616158c7e3da76b92a99cb82c65c513e3ca6
jovo0 <jonasvogelgesang.jv@gmail.com>
2026-06-07 23:06:46 +0200

    Produktdetails hinzugefügt und Drag & Drop initialisiert

--- scripts/product.gd
+++ scripts/product.gd
@@ -1,12 +1,34 @@
-extends Node2D
+extends Area2D
 
 @export var product_id: int = 0
 @export var product_name: String = ""
-
 @export var allergens: Array[String] = []
 @export var tags: Array[String] = []
 
-@onready var name_label = $NameLabel
+@onready var name_label: Label = $NameLabel
 
-func _ready():
+var dragging := false
+var active_touch_index := -1
+var drag_offset := Vector2.ZERO
+var start_position := Vector2.ZERO
+
+func _ready() -> void:
 	name_label.text = product_name
+	start_position = global_position
+
+func _input_event(viewport, event, shape_idx) -> void:
+	if event is InputEventScreenTouch:
+		if event.pressed and not dragging:
+			dragging = true
+			active_touch_index = event.index
+			drag_offset = global_position - event.position
+			z_index = 10
+
+		elif not event.pressed and event.index == active_touch_index:
+			dragging = false
+			active_touch_index = -1
+			z_index = 0
+
+	if event is InputEventScreenDrag:
+		if dragging and event.index == active_touch_index:
+			global_position = event.position + drag_offset
