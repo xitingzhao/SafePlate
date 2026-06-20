edc4eafcb86a592e37c006f2e4e670f5d9aad7ef
xiting.zhao <tina102023@163.com>
2026-06-20 17:36:30 +0200

    Profilauswahl_ui.version1

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,15 +1,21 @@
 extends Control
 
+@onready var title: Label = $MarginContainer/VBoxContainer/GameTitle
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	start_title_animation()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+func start_title_animation():
+	await get_tree().process_frame
+	title.pivot_offset = title.size / 2
+	var float_speed := 0.6  
+	var scale_speed := 0.6
+	var tween = create_tween().set_loops().set_parallel(true)
+	tween.tween_property(title, "position:y", title.position.y - 10, float_speed)\
+		.set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+	tween.chain().tween_property(title, "position:y", title.position.y, float_speed)\
+		.set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
 #Scene change from main menu to profile auswahl
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/menu/profilauswahl.tscn")
--- scripts/profilauswahl.gd
+++ scripts/profilauswahl.gd
@@ -1,17 +1,65 @@
 extends Control
 
+@onready var s1_cards: HBoxContainer = $S1/PanelContainer/VBoxContainer/HBoxContainer
+@onready var s2_cards: HBoxContainer = $S2/PanelContainer/VBoxContainer/HBoxContainer
+@onready var s1_ready_button: Button = $S1/PanelContainer/VBoxContainer/S1Button
+@onready var s2_ready_button: Button = $S2/PanelContainer/VBoxContainer/S2Button
+
+var is_s1_ready: bool = false
+var is_s2_ready: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	s1_ready_button.toggle_mode = true
+	s2_ready_button.toggle_mode = true
+	s1_ready_button.toggled.connect(_on_s1_ready_toggled)
+	s2_ready_button.toggled.connect(_on_s2_ready_toggled)
+	s1_ready_button.text = "Bereit?"
+	s2_ready_button.text = "Bereit?"
+	
+	for card in s1_cards.get_children():
+		card.pressed.connect(func():_on_s1_card_selected(card))
+	for card in s2_cards.get_children():
+		card.pressed.connect(func():_on_s2_card_selected(card))
 
+func _on_s1_card_selected(selected_card: Button):
+	if is_s1_ready:
+		return
+	GameGlobal.player1 = selected_card.text
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_s2_card_selected(selected_card: Button):
+	if is_s1_ready:
+		return
+	GameGlobal.player2 = selected_card.text
 
-#change scene from profile auswahl to game
-func _on_start_button_pressed() -> void:
-	GameGlobal.player1 = $VBoxContainer/S1Panel/S1OptionButton.get_item_text($VBoxContainer/S1Panel/S1OptionButton.selected)
-	GameGlobal.player2 = $VBoxContainer/S2Panel/S2OptionButton.get_item_text($VBoxContainer/S2Panel/S2OptionButton.selected)
-	get_tree().change_scene_to_file("res://scenes/game/GameScene.tscn")
+func _on_s1_ready_toggled(is_pressed:bool):
+	if is_pressed and (GameGlobal.player1 == "" or GameGlobal.player1 == null):
+		s1_ready_button.set_pressed_no_signal(false)
+		return
+	is_s1_ready = is_pressed
+	if is_pressed:
+		s1_ready_button.text = "Bereit!"
+		s1_ready_button.modulate = Color.GREEN
+	else:
+		s1_ready_button.text = "Bereit?"
+		s1_ready_button.modulate = Color.WHITE
+	check_both_ready()
+
+func _on_s2_ready_toggled(is_pressed: bool):
+	if is_pressed and (GameGlobal.player2 == "" or GameGlobal.player2 == null):
+		s2_ready_button.set_pressed_no_signal(false)
+		return
+	is_s2_ready = is_pressed
+	if is_pressed:
+		s2_ready_button.text = "Bereit!"
+		s2_ready_button.modulate = Color.GREEN
+	else:
+		s2_ready_button.text = "Bereit?"
+		s2_ready_button.modulate = Color.WHITE
+		
+	check_both_ready()
+	
+func check_both_ready():
+	if is_s1_ready and is_s2_ready:
+		await get_tree().create_timer(0.5).timeout
+		get_tree().change_scene_to_file("res://scenes/game/GameScene.tscn")
